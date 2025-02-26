dc7e4e09a341709b5c769bdce5b73cf4ca6834ab
Sten-Eric <154225748+Sten-Eric@users.noreply.github.com>
2025-02-26 11:25:37 +0200

    doubleint
    x

---
+++ datatypes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int bignumber = 123456789;
+                Console.WriteLine(bignumber);
+            //komaga number
+            double comanumber = 12.5;
+            Console.WriteLine(comanumber);
+        }
+    }
+}
